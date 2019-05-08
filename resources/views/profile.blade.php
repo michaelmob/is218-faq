77a99575dc19e34ab93cb0fe519c916dde3ce3d8
thetarkus <mike@mikemob.com>
2019-05-08 03:28:04 -0400

    Add profile views

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,16 +5,17 @@
     <div class="row ">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ $user->name }}'s Profile</div>
-
+                <div class="card-header">{{ $profile->user->name }}'s Profile</div>
                 <div class="card-body ">
                     <span class="font-weight-bold">First Name:</span> {{ $profile->fname }}</br>
                     <span class="font-weight-bold">Last Name: </span>{{ $profile->lname }}</br>
                     <span class="font-weight-bold">Body: </span>{{ $profile->body }}</br>
                 </div>
                 <div class="card-footer">
+                    <a class="btn btn-success float-right" href="{{ route('profiles.edit', [ 'id' => $profile->user->id ]) }}">
+                        Edit
+                    </a>
                 </div>
-
             </div>
         </div>
     </div>
