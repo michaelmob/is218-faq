13a98f8e4a0dfa80de1be99552b7f44a7128e0ab
thetarkus <mike@mikemob.com>
2019-05-07 23:21:13 -0400

    Add Question creation test

---
+++ tests/Unit/QuestionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class QuestionTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testQuestionCreation()
+    {
+        $user = factory(\App\User::class)->create();
+
+        $question = factory(\App\Question::class)->make();
+        $question->user()->associate($user);
+        $questionCreated = $question->save();
+
+        $this->assertTrue($questionCreated);
+    }
+}
