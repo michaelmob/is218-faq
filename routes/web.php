231e2c314b6bd7856e21016112d2ee73ed9b3f00
thetarkus <mike@mikemob.com>
2019-05-08 03:44:32 -0400

    Change default name and pagination styling

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('profiles', 'ProfileController');
